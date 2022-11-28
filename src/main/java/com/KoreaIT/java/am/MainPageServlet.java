af9369da10daf331deb440cd8429a1b5fb62b656
yemyoungwoo <tkddlektks@gmail.com>
2022-11-28 20:04:41 +0900

    로그인, 로그아웃 기능구현

--- src/main/java/com/KoreaIT/java/am/MainPageServlet.java
+++ src/main/java/com/KoreaIT/java/am/MainPageServlet.java
@@ -1,5 +1,4 @@
 package com.KoreaIT.java.am;
-
 import java.io.IOException;
 
 import jakarta.servlet.ServletException;
@@ -7,14 +6,32 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import jakarta.servlet.http.HttpSession;
 @WebServlet("/home/main")
 public class MainPageServlet extends HttpServlet {
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+
+		boolean isLogined = false;
+		int loginedMemberId = -1;
+
+		if(session.getAttribute("loginedMemberLoginId") != null) {
+			loginedMemberId = (int) session.getAttribute("loginedMemberId");
+			isLogined = true;
+		}
+
+		request.setAttribute("isLogined", isLogined);
+		request.setAttribute("loginedMemberId", loginedMemberId);
 
 		request.getRequestDispatcher("/jsp/home/main.jsp").forward(request, response);
 
 	}
-
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 }
